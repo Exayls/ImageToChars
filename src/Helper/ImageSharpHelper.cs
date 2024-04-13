44a71f2e0c9252b9366073cccfe8886843e96671
Alexis Gagoud <alexis.gagoud@hotmail.fr>
2024-04-14 00:43:12 +0200

    get Array from Image

---
+++ src/Helper/ImageSharpHelper.cs
@@ -0,0 +1,24 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Helper
+{
+    public class ImageSharp
+    {
+        static public Rgba32[,] GetPixelArray(Image<Rgba32> image)
+        {
+            var height = image.Height;
+            var width = image.Width;
+            var array = new Rgba32[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Rgba32 rgba32 = image[x, y];
+                    array[x, y] = rgba32;
+                }
+            }
+            return array;
+        }
+    }
+}
--- test/ImageToChars.Tests/Tests.fs
+++ test/ImageToChars.Tests/Tests.fs
@@ -1,10 +1,18 @@
 module Tests
 
+
 open System
 open Xunit
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open SixLabors.ImageSharp.PixelFormats
+open Helper
 
-let GetArrayFrom (image: Image) : int[,]= 
-    array2D [[]]
+let GetArrayFrom (image: Image<Rgba32>) : float[,]= 
+    let pixels = Helper.ImageSharp.GetPixelArray image
+    let ComputeIntensity (a:Rgba32) = float ((int a.R + int a.G + int a.B)/(3*255))
+    let intensityArray = Array2D.map ComputeIntensity pixels
+    intensityArray
 
 let AreEquals (arr1: 'a [,]) (arr2: 'a [,]) : bool =
     if arr1.GetLength(0) <> arr2.GetLength(0) || arr1.GetLength(1) <> arr2.GetLength(1) then
@@ -14,6 +22,18 @@ let AreEquals (arr1: 'a [,]) (arr2: 'a [,]) : bool =
         let cols = arr1.GetLength(1)
         let indices = seq { for i in 0 .. rows - 1 do for j in 0 .. cols - 1 -> (i, j) }
         not (Seq.exists (fun (i, j) -> arr1.[i, j] <> arr2.[i, j]) indices)
+
+let AreEqualsWithAcc (arr1: float [,]) (arr2: float [,]) (acc: float) : bool =
+    if arr1.GetLength(0) <> arr2.GetLength(0) || arr1.GetLength(1) <> arr2.GetLength(1) then
+        false
+    else
+        let rows = arr1.GetLength(0)
+        let cols = arr1.GetLength(1)
+        let indices = seq { for i in 0 .. rows - 1 do for j in 0 .. cols - 1 -> (i, j) }
+        not (Seq.exists (fun (i, j) -> arr1.[i, j] - arr2.[i, j] > acc) indices)
+
+let Transpose array2d= 
+    Array2D.init (array2d |> Array2D.length2) (array2d |> Array2D.length1) (fun r c -> array2d.[c,r])
 
 let map2 (func:'a -> 'a -> 'a) (array1:'a[,]) (array2:'a[,]) = 
     if array1.GetLength(0) <> array2.GetLength(0) || array1.GetLength(1) <> array2.GetLength(1) then
@@ -28,7 +48,7 @@ let map2 (func:'a -> 'a -> 'a) (array1:'a[,]) (array2:'a[,]) =
 
 [<Fact>]
 let ``when_arrays_are_equals_AreEquals_should_returns_true`` () =
-    let arr1: int[,] =
+    let arr1: float[,] =
         array2D
             [ [ 0; 0; 0 ]
               [ 1; 1; 1 ]
@@ -37,7 +57,7 @@ let ``when_arrays_are_equals_AreEquals_should_returns_true`` () =
               [ 0; 1; 0 ]
               [ 0; 0; 0 ] ]
 
-    let arr2: int[,] =
+    let arr2: float[,] =
         array2D
             [ [ 0; 0; 0 ]
               [ 1; 1; 1 ]
@@ -52,7 +72,7 @@ let ``when_arrays_are_equals_AreEquals_should_returns_true`` () =
 
 [<Fact>]
 let ``when_arrays_are_equals_AreEquals_should_returns_false`` () =
-    let arr1: int[,] =
+    let arr1: float[,] =
         array2D
             [ [ 0; 0; 0 ]
               [ 1; 1; 1 ]
@@ -61,7 +81,7 @@ let ``when_arrays_are_equals_AreEquals_should_returns_false`` () =
               [ 0; 1; 0 ]
               [ 0; 0; 0 ] ]
 
-    let arr2: int[,] =
+    let arr2: float[,] =
         array2D
             [ [ 0; 0; 0 ]
               [ 1; 1; 0 ]
@@ -72,4 +92,20 @@ let ``when_arrays_are_equals_AreEquals_should_returns_false`` () =
 
     let equality = AreEquals arr2 arr1
     Assert.False(equality)
+
+[<Fact>]
+let ``when_calling_GetArray_should_get_image_coresponding`` () =
+    let TFilter: float[,] =
+        Transpose (
+        array2D
+            [ [ 1; 1; 1 ]
+              [ 0; 0; 0 ]
+              [ 1; 0; 1 ]
+              [ 1; 0; 1 ]
+              [ 1; 0; 1 ]
+              [ 1; 1; 1 ] ])
+    let image = Image.Load<Rgba32>("ressources/T.png")
+    let imageArray = GetArrayFrom(image)
+    printf "%A" (Transpose imageArray)
+    Assert.True(AreEqualsWithAcc imageArray TFilter 0.1)
 
